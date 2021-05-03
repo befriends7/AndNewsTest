d542af2145d559d95e671e492a1f4a518ff4b16f
Rovero <jeugenejohn@yahoo.com>
2021-05-03 20:58:23 +0530

    Initial commit

---
+++ app/src/main/java/com/rovero/andtest/flashScreen.kt
@@ -0,0 +1,11 @@
+package com.rovero.andtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class flashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_screen)
+    }
+}
