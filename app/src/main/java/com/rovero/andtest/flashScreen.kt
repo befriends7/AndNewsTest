dacfd72af0122953a5951828178fd3b540859a1a
Rovero <jeugenejohn@yahoo.com>
2021-05-03 21:11:34 +0530

    new change

--- app/src/main/java/com/rovero/andtest/flashScreen.kt
+++ app/src/main/java/com/rovero/andtest/flashScreen.kt
@@ -2,10 +2,19 @@ package com.rovero.andtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
+lateinit var imgMain:ImageView;
 
 class flashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_screen)
+
+        imgMain = findViewById(R.id.imgMain)
+
+        Picasso.get().load(R.drawable.fireb).fit().into(imgMain)
+
     }
 }
